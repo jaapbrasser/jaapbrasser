d2c4e9d77261b913d3006611efe63af88b946128
Jaap Brasser <noreply@jaapbrasser.com>
2020-07-10 16:05:51 +0200

    Updated changeimage.ps1

--- .github/actions/scripts/changeimage.ps1
+++ .github/actions/scripts/changeimage.ps1
@@ -1,4 +1,6 @@
 $RandomFile = Get-ChildItem -Path ./Assets -Exclude *md | Get-Random | Select-Object -ExpandProperty Name
+Write-Output "The new file is: '$RandomFile'"
 $env:filename = $RandomFile
+Write-Output "::set-env name=filename::$($env:filename)"
 (Get-Content ./README.md) -replace '(/Assets/).*?(\")', ('$1{0}$2' -f $RandomFile) |
     Set-Content -Path ./README.md
