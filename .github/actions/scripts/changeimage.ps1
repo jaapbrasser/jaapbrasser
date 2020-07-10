43b1bb2f20ef6af64f7eecd5e896e0c0e3914388
Jaap Brasser <noreply@jaapbrasser.com>
2020-07-10 14:36:36 +0200

    Made some changes here

--- .github/actions/scripts/changeimage.ps1
+++ .github/actions/scripts/changeimage.ps1
@@ -1,1 +1,3 @@
-echo "Hello Jaap"
+$RandomFile = Get-ChildItem -Path ./Assets | Get-Random | Select-Object -ExpandProperty Name
+(Get-Content ./README.md) -replace '(/Assets/).*?(\")', ('$1{0}$2' -f $RandomFile) |
+    Set-Content -Path ./README.md
