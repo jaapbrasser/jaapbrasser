6e3e9b29891bb2696e8d46234ac88c170b8fc72d
Jaap Brasser <jcjbrasser@gmail.com>
2020-11-29 15:59:33 +0100

    Update changeimage.ps1
    Added environment var

--- .github/actions/scripts/changeimage.ps1
+++ .github/actions/scripts/changeimage.ps1
@@ -3,13 +3,6 @@ $RandomFile = Get-ChildItem -Path ./Assets -Exclude *md |
     Where-Object {$_.Name -ne $CurrentFile} |
     Get-Random | Select-Object -ExpandProperty Name
 Write-Output "The new file is: '$RandomFile'"
-$env:filename = $RandomFile
-gci $env:GITHUB_PATH | select fullname
-gci $env:GITHUB_PATH | % {
-'*'*60
-$_.fullname
-cat $_
-"-"*60
-}
+Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append -InputObject "filename=$RandomFile"
 (Get-Content ./README.md) -replace '(/Assets/).*?(\")', ('$1{0}$2' -f $RandomFile) |
     Set-Content -Path ./README.md
